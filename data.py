c06fdf6cbdfcf26b9459f3531dcd7f3936b49ada
kane <Kane.Li@movit-tech.com>
2025-04-22 18:06:45 +0800

    init

---
+++ data.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+counter = 100          # 整型变量
+miles   = 1000.0       # 浮点型变量
+name    = "runoob"     # 字符串
+
+print (counter)
+print (miles)
+print (name)
+
+a = b = c = 1
+
+# 变量定义
+x = 10          # 整数
+y = 3.14         # 浮点数
+name = "Alice"   # 字符串
+is_active = True # 布尔值
+
+# 多变量赋值
+a, b, c = 1, 2, "three"
+
+# 查看数据类型
+print(type(x))        # <class 'int'>
+print(type(y))        # <class 'float'>
+print(type(name))     # <class 'str'>
+print(type(is_active)) # <class 'bool'>
