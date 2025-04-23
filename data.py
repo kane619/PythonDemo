3c3136fdc929c7391d61804ce20dccb6666579fd
kane <Kane.Li@movit-tech.com>
2025-04-23 10:21:43 +0800

    基本数据类型

--- data.py
+++ data.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+from unittest import result
+
+
 counter = 100          # 整型变量
 miles   = 1000.0       # 浮点型变量
 name    = "runoob"     # 字符串
@@ -24,3 +27,194 @@ print(type(x))        # <class 'int'>
 print(type(y))        # <class 'float'>
 print(type(name))     # <class 'str'>
 print(type(is_active)) # <class 'bool'>
+
+a = 111
+result = isinstance(a, int)
+print(f"result===: {result}")
+
+# Python3 中，bool 是 int 的子类，True 和 False 可以和数字相加， True==1、False==0 会返回 True，但可以通过 is 来判断类型。
+result2 = issubclass(bool, int)
+print(f"result2===: {result2}")
+
+result3 = True + 1
+print(f"result3===: {result3}")
+result4 = True is 1
+print(f"result4===: {result4}")
+
+print('Ru\noob')
+print(r'Ru\noob')
+# 与 C 字符串不同的是，Python 字符串不能被改变。向一个索引位置赋值，比如 word[0] = 'm' 会导致错误。
+
+# 布尔类型的值和类型
+a = True
+b = False
+print(type(a))  # <class 'bool'>
+print(type(b))  # <class 'bool'>
+
+# 布尔类型的整数表现
+print(int(True))   # 1
+print(int(False))  # 0
+
+# 使用 bool() 函数进行转换
+print(bool(0))         # False
+print(bool(42))        # True
+print(bool(''))        # False
+print(bool('Python'))  # True
+print(bool([]))        # False
+print(bool([1, 2, 3])) # True
+
+# 布尔逻辑运算
+print(True and False)  # False
+print(True or False)   # True
+print(not True)        # False
+
+# 布尔比较运算
+print(5 > 3)  # True
+print(2 == 2) # True
+print(7 < 4)  # False
+
+# 布尔值在控制流中的应用
+if True:
+    print("This will always print")
+
+if not False:
+    print("This will also always print")
+
+x = 10
+if x:
+    print("x is non-zero and thus True in a boolean context")
+
+# List(列表)
+#!/usr/bin/python3
+
+list = [ 'abcd', 786 , 2.23, 'runoob', 70.2 ]  # 定义一个列表
+tinylist = [123, 'runoob']
+
+print (list)            # 打印整个列表
+print (list[0])         # 打印列表的第一个元素
+print (list[1:3])       # 打印列表第二到第四个元素（不包含第四个元素）
+print (list[2:])        # 打印列表从第三个元素开始到末尾
+print (tinylist * 2)    # 打印tinylist列表两次
+print (list + tinylist)  # 打印两个列表拼接在一起的结果
+
+a = [1, 2, 3, 4, 5, 6]
+a[0] = 9
+a[2:5] = [13, 14, 15]
+print(a)
+a[2:5] = []   # 将对应的元素值设置为 [] #
+print(a)
+
+def reverseWords(input):
+
+    # 通过空格将字符串分隔符，把各个单词分隔为列表
+    inputWords = input.split(" ")
+
+    # 翻转字符串
+    # 假设列表 list = [1,2,3,4],
+    # list[0]=1, list[1]=2 ，而 -1 表示最后一个元素 list[-1]=4 ( 与 list[3]=4 一样)
+    # inputWords[-1::-1] 有三个参数
+    # 第一个参数 -1 表示最后一个元素
+    # 第二个参数为空，表示移动到列表末尾
+    # 第三个参数为步长，-1 表示逆向
+    inputWords=inputWords[-1::-1]
+
+    # 重新组合字符串
+    output = ' '.join(inputWords)
+
+    return output
+
+if __name__ == "__main__":
+    input = 'I like runoob'
+    rw = reverseWords(input)
+    print(rw)
+
+#!/usr/bin/python3
+#  元组（tuple）与列表类似，不同之处在于元组的元素不能修改
+tuple = ( 'abcd', 786 , 2.23, 'runoob', 70.2  )
+tinytuple = (123, 'runoob')
+
+print (tuple)             # 输出完整元组
+print (tuple[0])          # 输出元组的第一个元素
+print (tuple[1:3])        # 输出从第二个元素开始到第三个元素
+print (tuple[2:])         # 输出从第三个元素开始的所有元素
+print (tinytuple * 2)     # 输出两次元组
+print (tuple + tinytuple) # 连接元组
+
+# Set（集合）
+# Python 中的集合（Set）是一种无序、可变的数据类型，用于存储唯一的元素。
+
+# 集合中的元素不会重复，并且可以进行交集、并集、差集等常见的集合操作。
+
+# 在 Python 中，集合使用大括号 {} 表示，元素之间用逗号 , 分隔。
+
+# 另外，也可以使用 set() 函数创建集合。
+sites = {'Google', 'Taobao', 'Runoob', 'Facebook', 'Zhihu', 'Baidu'}
+
+print(sites)   # 输出集合，重复的元素被自动去掉
+
+# 成员测试
+if 'Runoob' in sites :
+    print('Runoob 在集合中')
+else :
+    print('Runoob 不在集合中')
+
+
+# set可以进行集合运算
+a = set('abracadabra')
+b = set('alacazam')
+
+print(a)
+
+print(a - b)     # a 和 b 的差集
+
+print(a | b)     # a 和 b 的并集
+
+print(a & b)     # a 和 b 的交集
+
+print(a ^ b)     # a 和 b 中不同时存在的元素
+
+# Dictionary（字典）
+# 字典（dictionary）是Python中另一个非常有用的内置数据类型。
+
+# 列表是有序的对象集合，字典是无序的对象集合。两者之间的区别在于：字典当中的元素是通过键来存取的，而不是通过偏移存取。
+
+# 字典是一种映射类型，字典用 { } 标识，它是一个无序的 键(key) : 值(value) 的集合。
+
+# 键(key)必须使用不可变类型。
+
+# 在同一个字典中，键(key)必须是唯一的。
+
+dict = {}
+dict['one'] = "1 - 菜鸟教程"
+dict[2]     = "2 - 菜鸟工具"
+
+tinydict = {'name': 'runoob','code':1, 'site': 'www.runoob.com'}
+
+
+print (dict['one'])       # 输出键为 'one' 的值
+print (dict[2])           # 输出键为 2 的值
+print (tinydict)          # 输出完整的字典
+print (tinydict.keys())   # 输出所有键
+print (tinydict.values()) # 输出所有值
+
+
+# 在 Python3 中，bytes 类型表示的是不可变的二进制序列（byte sequence）。
+
+# 与字符串类型不同的是，bytes 类型中的元素是整数值（0 到 255 之间的整数），而不是 Unicode 字符。
+
+# bytes 类型通常用于处理二进制数据，比如图像文件、音频文件、视频文件等等。在网络编程中，也经常使用 bytes 类型来传输二进制数据。
+
+# 创建 bytes 对象的方式有多种，最常见的方式是使用 b 前缀：
+
+# 此外，也可以使用 bytes() 函数将其他类型的对象转换为 bytes 类型。bytes() 函数的第一个参数是要转换的对象，第二个参数是编码方式，如果省略第二个参数，则默认使用 UTF-8 编码：
+
+x = bytes("hello", encoding="utf-8")
+# 与字符串类型类似，bytes 类型也支持许多操作和方法，如切片、拼接、查找、替换等等。同时，由于 bytes 类型是不可变的，因此在进行修改操作时需要创建一个新的 bytes 对象。例如：
+
+x = b"hello"
+y = x[1:3]  # 切片操作，得到 b"el"
+z = x + b"world"  # 拼接操作，得到 b"helloworld"
+
+x = b"hello"
+if x[0] == ord("h"):
+    print("The first element is 'h'")
